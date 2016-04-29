d62b6f97b11e742507a05b0d2370d137d7279ba9
BatlomBlu <szelis@wp.pl>
2016-04-29 10:55:35 +0200

    Delete Zad02.c

--- Lab07/Zad02.c
+++
@@ -1,5 +0,0 @@
-  #define INFO(kontynent, gora) Najwyższym szczytem kontynent jest gora.
-  INFO(Azji, Mt. Everest)
-  INFO(Europy, Elbrus)
-  INFO(Afryki, Kilimandzaro)
-  INFO(Australii, Gora Kosciuszki)
