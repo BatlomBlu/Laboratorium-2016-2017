3bc83b05dcb1f01e0889a86fbcbf0b20759a448c
BatlomBlu <szelis@wp.pl>
2016-05-06 11:03:49 +0200

    Update Zad01.c

--- Lab07/Zad01.c
+++ Lab07/Zad01.c
@@ -6,16 +6,12 @@ int main (int count, char* arg[ ]) {
   int i;
   
   for(i=count; i>1; i--){
-  	
-    if(i==count){
+    if(i==count)
       printf("%s", arg[i-1]);
-    }
-    
     else
       printf(" %s", arg[i-1]);
-      
   }
-    
-printf("\n");
+  printf("\n");
+
 return 0;
 }
