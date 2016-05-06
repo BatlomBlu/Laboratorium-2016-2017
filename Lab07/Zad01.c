40248f2c9543853168e93937e1317d45fb53d69d
BatlomBlu <szelis@wp.pl>
2016-05-06 11:33:16 +0200

    Update Zad01.c

--- Lab07/Zad01.c
+++ Lab07/Zad01.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main (int count, char* arg[ ]) {
+void write_from_end(char* arg[ ], int count, char separator);
+
+
+int main (int count, char* arg[ ]) {	
 	
-  int i;
-  
-  for(i=count; i>1; i--){
-    if(i==count)
-      printf("%s", arg[i-1]);
-    else
-      printf(" %s", arg[i-1]);
-  }
-  printf("\n");
+  write_from_end(arg,count,' ');
+  return 0;
+}
 
-return 0;
+
+void write_from_end(char* arg[ ], int count, char separator){
+  
+  int i=count;
+  
+  printf("%s", arg[i-1]);
+  for(i=count-1; i>1; i--){	
+      printf("%c", separator);
+      printf("%s", arg[i-1]);      
+  }  
+  printf("\n");
 }
